355e0a1175610bb798cf9a7acb401c70f52144ea
Somtochukwu Nweke <somtonweke1@fb.com>
2018-06-27 11:11:09 -0700

    Initial Commit

---
+++ MyProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyProject
+//
+//  Created by Somtochukwu Nweke on 6/27/18.
+//  Copyright © 2018 Somtochukwu Nweke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyProject
+//
+//  Created by Somtochukwu Nweke on 6/27/18.
+//  Copyright © 2018 Somtochukwu Nweke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
